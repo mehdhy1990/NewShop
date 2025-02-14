bc7b427fa117937494a0d9966002c7ac1da139f6
Mehdi Heidari <hymehdi84@gmail.com>
2025-02-14 19:52:23 +0100

    first migration added

--- BethanysPieShopAdmin/obj/Debug/net9.0/BethanysPieShopAdmin.AssemblyInfo.cs
+++ BethanysPieShopAdmin/obj/Debug/net9.0/BethanysPieShopAdmin.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BethanysPieShopAdmin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9c16dab1696e84695c340fe78db97b9833b0c5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+049d5ab6bd865d0074990251e17dac5e11de3b96")]
 [assembly: System.Reflection.AssemblyProductAttribute("BethanysPieShopAdmin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BethanysPieShopAdmin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
